051bd1f4f99b1de3da65352af42a0de92313ab42
Luca Fischer <lucafischer@lucas-laptop.speedport.ip>
2024-03-12 14:11:34 +0100

    Add revalidate: 0 to press

--- src/app/pages/press/page.tsx
+++ src/app/pages/press/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import request from "graphql-request";
 import type { ClippingType } from "@/types/ClippingTypes";
 import { ClippingSchema } from "@/types/ClippingTypes";
 import type { Metadata } from "next";
@@ -12,18 +11,12 @@ export const metadata: Metadata = {
 
 const WP_GRAPHQL_BASE = process.env.WP_GRAPHQL_BASE!;
 
-// Renews Cache for this route every 600 seconds
-export const revalidate = 10;
-// type Props = {
-//   pressClippings : ClippingType[],
-// };
-
 export default async function Clippings() {
   const pressClippings = await getClippings();
   
   return (
     <main className="my-auto">
-      
+      <p>hello</p>
       {pressClippings
         .sort(
           (a, b) =>
@@ -37,78 +30,82 @@ export default async function Clippings() {
   );
 }
 
-// async function getClippings() {
-//   const query = `
-//     {
-//       pressClippings {
-//         nodes {
-//           pressLink
-//           title
-//           id
-//           pressAuthor
-//           pressDate
-//         }
-//       }
-//     }
-//   `;
-
-//   try {
-//     const response = await fetch(WP_GRAPHQL_BASE, {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({ query }),
-//     });
-
-//     if (!response.ok) {
-//       throw new Error("Failed to fetch press clippings");
-//     }
-//     console.log(response);
-
-//     const data = await response.json();
-
-//     const clippings: ClippingType[] = data.data.pressClippings.nodes.map(
-//       (clipping: any) => ClippingSchema.parse(clipping)
-//     );
-
-//     return clippings;
-//   } catch (error) {
-//     console.error("Error fetching press clippings:", error);
-//     return [];
-//   }
-// }
-
-
 async function getClippings() {
   const query = `
-	{
-		pressClippings {
-			nodes {
-			  pressLink
-			  title
-			  id
-			  pressAuthor
-			  pressDate
-			}
-		  }
-	  }
-	`;
+    {
+      pressClippings {
+        nodes {
+          pressLink
+          title
+          id
+          pressAuthor
+          pressDate
+        }
+      }
+    }
+  `;
 
   try {
-    const response = (await request(WP_GRAPHQL_BASE, query)) as {
-      pressClippings: {
-        nodes: ClippingType[];
-      };
-    };
+    const response = await fetch(WP_GRAPHQL_BASE, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query }),
+      next: {
+        revalidate: 0,
+      },
+      // cache: "no-store"
+    });
 
-    const clippings: ClippingType[] = response.pressClippings.nodes.map(
-      (clipping) => ClippingSchema.parse(clipping)
+    if (!response.ok) {
+      throw new Error("Failed to fetch press clippings");
+    }
+    console.log(response);
+
+    const data = await response.json();
+
+    const clippings: ClippingType[] = data.data.pressClippings.nodes.map(
+      (clipping: any) => ClippingSchema.parse(clipping)
     );
-    // console.log(clippings);
+
     return clippings;
   } catch (error) {
     console.error("Error fetching press clippings:", error);
     return [];
   }
 }
+
+
+// async function getClippings() {
+//   const query = `
+// 	{
+// 		pressClippings {
+// 			nodes {
+// 			  pressLink
+// 			  title
+// 			  id
+// 			  pressAuthor
+// 			  pressDate
+// 			}
+// 		  }
+// 	  }
+// 	`;
+
+//   try {
+//     const response = (await request(WP_GRAPHQL_BASE, query)) as {
+//       pressClippings: {
+//         nodes: ClippingType[];
+//       };
+//     };
+
+//     const clippings: ClippingType[] = response.pressClippings.nodes.map(
+//       (clipping) => ClippingSchema.parse(clipping)
+//     );
+//     // console.log(clippings);
+//     return clippings;
+//   } catch (error) {
+//     console.error("Error fetching press clippings:", error);
+//     return [];
+//   }
+// }
